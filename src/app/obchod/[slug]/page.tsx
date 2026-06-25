44b3c71ff90faa6b811d4fa8f5f56425996d5a1c
balintj4 <balintjanik04@gmail.com>
2026-06-25 23:24:32 +0200

    Edited shop layout

--- src/app/obchod/[slug]/page.tsx
+++ src/app/obchod/[slug]/page.tsx
@@ -3,6 +3,7 @@ import { createClient } from "@/lib/server";
 import CategoryHeader from "@/components/ui/categoryHeader";
 import ProductCard from "@/components/ui/productCard";
 import { getProdCat } from "@/managers/getProductsByCategory";
+import { Separator } from "@/components/ui/separator";
 
 export interface Product {
   id: number;
@@ -15,7 +16,7 @@ export default async function CategoryPage({
   const { slug } = await params;
   const supabase = await createClient();
   const products = await getProdCat(slug);
-  console.log(products);
+  const {data: catName} = await supabase.from('categories').select('name').eq('slug',slug).single()
 
   if (!products) {
     return (
@@ -29,7 +30,15 @@ export default async function CategoryPage({
   return (
     <>
       <CategoryHeader />
-      <div className="grid grid-cols-[repeat(auto-fill,minmax(250px,1fr))] gap-4">
+      {catName ?
+      <div className="ml-12 mt-6 mb-2 flex flex-row gap-2 items-center">
+        <h1 className="text-3xl">Obchod:</h1>
+        <h2 className="text-2xl font-bold">{catName.name}</h2>
+      </div> :
+         <h1 className="">Obchod</h1>
+      }
+      <Separator orientation="horizontal" className="bg-secondary mb-2 max-w-[1350px] mx-auto min-h-[2px]" />
+      <div className="p-12 grid grid-cols-[repeat(auto-fill,minmax(200px,1fr))] gap-8">
         {products.map((product: Product) => (
           <ProductCard
             key={product.id}
